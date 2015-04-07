d5435249e39aa3ad642acd4c3f10887c52a59913
May Miller-Ricci <maymillerricci@gmail.com>
2015-04-06 17:10:11 -0700

    add pseudocode for tests outlining steps to take

--- spec/features/request_help_spec.rb
+++ spec/features/request_help_spec.rb
@@ -8,3 +8,21 @@ describe "request help" do
   end
 
 end
+
+# student view should have "Request Help From My Teacher" button
+# when student clicks button
+  # button disappears
+  # indicator saying teacher has been notified appears
+
+# teacher view
+  # if no students have requested help
+    # no alert that students have requested help
+  # if any students have requested help
+    # alert shows students who have requested help in the past day,
+      # along with time help was requested,
+      # along with option to clear help request for that student
+    # TODO: how to deal with requests that last longer than a day without being cleared?
+
+# when teacher clears help request (after talking to student)
+  # student disappears from teacher dashboard alert
+  # button to request help shows up again for student
