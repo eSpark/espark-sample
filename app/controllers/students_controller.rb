b0c087eee89076500256d487eadb474ed1cdabc7
Alex Koppel <alex@alexkoppel.com>
2015-04-21 19:55:50 -0500

    Expand our eSpark Challenge

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,16 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+    @has_math_request = @student.help_requests.where(subject: "Math").count > 0
+    @has_english_request = @student.help_requests.where(subject: "English").count > 0
   end
 
+  def ask_for_help
+    request = HelpRequest.new(student_id: params[:id], subject: params[:subject])
+    if request.save
+      render json: {created: true}, status: 200
+    else
+      render json: {error: true}, status: 422
+    end
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,3 @@
 class Student < ActiveRecord::Base
+  has_many :help_requests
 end
